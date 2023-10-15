214dac8bf40bc3342cfb98b0358bea14845b9a1c
Mahak Gupta <mahak03.gupta@gmail.com>
2023-10-15 16:53:48 +0530

    Commit

--- shareit/settings.py
+++ shareit/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-94(xuop^bhsd-!)t4l_i3-)tseg5ha%e*0o!@^-4i(8b&d%wtc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://shareit.up.railway.app/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
